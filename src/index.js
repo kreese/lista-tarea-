0764d462c0f9f8e19c43d56cac8785fba5c40f14
Cris <a02003865@utmir.edu.mx>
2021-08-13 01:25:18 -0500

    Inicio

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './styles.css';
+
+import { Todo, TodoList } from './classes';
+import {crearTodoHtml} from './js/componentes';
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(crearTodoHtml);
+
+/*const tarea = new Todo ('Aprende Boost');
+todoList.nuevoTodo(tarea);
+
+console.log(tarea);
+crearTodoHtml(tarea);*/
